348322506d9f6b247c4842999afb9424b9dcbcac
YourAverageMo <Yilmaz@arizona.edu>
2024-12-03 23:06:33 -0700

    add(components): header, footer, food

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Food from "./Food.jsx";
+import Footer from "./Footer.jsx";
+import Header from "./Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Food></Food>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
