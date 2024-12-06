96e1814fb187f6513df995e495c00141cb2a740e
YourAverageMo <Yilmaz@arizona.edu>
2024-12-05 22:02:06 -0700

    Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,14 @@
-import Card from "./Card.jsx";
+import Button from "./Button";
+import Student from "./Student";
+
 function App() {
   return (
     <>
-      <Card />
-      <Card />
+    <Student name="Spongebob" age={40} isStudent={true} />
+    <Student name="Patrick" age={42} isStudent={false} />
+    <Student name="Squidward" age={50} isStudent={false} />
+    <Student name="Sandy" age={27} isStudent={true} />
+    <Student name="larry"/>
     </>
   );
 }
